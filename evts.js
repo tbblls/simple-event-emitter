c9059049b2528e70ca1f9f4b1cea87ed6822b12d
Adam Tibballs <adam.tibballs@dss.gov.au>
2015-12-04 11:27:31 +1100

    first commit

---
+++ evts.js
@@ -0,0 +1,81 @@
+;(function(){
+  'use strict';
+  var Evts = {};
+  var events = [];
+  
+  function subscribe(name, elem, func){
+    elem.addEventListener(name, func, false);
+  }
+  
+  function addEvent(name){
+    return new CustomEvent(name);
+  }
+  
+  function getEventIndex(name){
+    return _.findIndex(events, function (event) {
+      return event.Name === name;
+    });
+  }
+  
+  function getEvent(name){
+    var index = getEventIndex(name);
+    if(index > -1){
+      return events[index];
+    }
+    else{
+      return null;
+    }
+  }
+  
+  var getArray = function(args){
+    return Array.prototype.slice.call(args);
+  }
+  
+  // add new event
+  Evts.AddListener = function(name, elem, func){
+    var listener = {
+      Event: addEvent(name),
+      Name: name,
+      Element: elem
+    };
+		var event = getEvent(name);
+    if(!event){
+      events.push(listener);
+      var args = getArray(arguments);
+      subscribe(...args);
+    }
+
+    return events;
+  }
+  
+  
+  // fire event
+  Evts.FireEvent = function(name){
+    try{
+      var event = getEvent(name);
+			if(event){
+        event.Element.dispatchEvent(event.Event);
+        return { success: true, message: 'Event has been fired.' };
+      }
+      else{
+        return { success: false, message: 'Event not found.' };
+      }
+      
+    }
+    catch(error){
+      return { success: false, message: 'Error occured while attempting to fire event.', error: error };
+    }
+    
+  }
+  
+  // remove event
+  Evts.RemoveListener = function(name){
+    var index = getEventIndex(name);		
+    events.splice(index, 1); 
+    return events;
+  }
+
+  // add to global
+  window.Evts = Evts;
+    
+}());
